ca8e9e2241c213d311bc47af43911d14777ec601
Shristi Pal <117811125+shristi7699@users.noreply.github.com>
2026-04-07 11:47:02 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -6,24 +6,33 @@ public class Main {
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
 
         int swaps = 0;
 
-        // Insertion Sort using swaps (NOT shifts)
-        for (int i = 1; i < n; i++) {
-            int j = i;
+        // Optimized Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
 
-            while (j > 0 && arr[j] < arr[j - 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = temp;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 
-                swaps++;
-                j--;
+                    swaps++;
+                    swapped = true;
+                }
             }
+
+            // Optimization: stop if already sorted
+            if (!swapped) break;
         }
 
+        // Print array
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             if (i > 0) sb.append(" ");
