72ea3976ddc1019948ab679b09268c150070eec2
Shristi Pal <117811125+shristi7699@users.noreply.github.com>
2026-04-07 11:36:07 +0530

    Implement selection sort in Main.java
    Replaced bubble sort with selection sort for improved efficiency.

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 public class Main {
@@ -6,17 +5,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+
         for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
         int swaps = 0;
+
+        // Selection Sort
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                    swaps++;
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
+            }
+
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+                swaps++;
             }
         }
 
@@ -25,6 +33,7 @@ public class Main {
             if (i > 0) sb.append(" ");
             sb.append(arr[i]);
         }
+
         System.out.println(sb);
         System.out.println("Swaps: " + swaps);
     }
