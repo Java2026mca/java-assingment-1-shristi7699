6afd84b95e95fbb605e36caff75468dfddff0d5b
Shristi Pal <117811125+shristi7699@users.noreply.github.com>
2026-04-07 10:53:52 +0530

    Implement Pascal's triangle generation and output

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -5,20 +5,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        int[][] pascal = new int[n][];
         for (int i = 0; i < n; i++) {
-            int num = 1;
-
-            for (int j = 0; j <= i; j++) {
-                System.out.print(num);
-
-                if (j < i) {
-                    System.out.print(" ");
-                }
-
-                // Calculate next value using formula
-                num = num * (i - j) / (j + 1);
+            pascal[i] = new int[i + 1];
+            pascal[i][0] = 1;
+            pascal[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
             }
-            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j <= i; j++) {
+                if (j > 0) sb.append(" ");
+                sb.append(pascal[i][j]);
+            }
+            System.out.println(sb);
         }
     }
 }
