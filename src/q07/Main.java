5b4a3770c42c6ff9f324e06fd412d3bd372cf131
Shristi Pal <117811125+shristi7699@users.noreply.github.com>
2026-04-07 11:02:25 +0530

    Add bubble sort implementation with swap count
    Implemented bubble sort to sort an array of integers and count swaps.

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -4,19 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
-        // TODO: Read n integers into an array
-        //       Implement BUBBLE SORT manually (no Arrays.sort)
-        //       Print sorted array ascending, space-separated
-        //       Then print: "Swaps: X" where X = total number of swaps performed
-        //
-        // Input:
-        // 5
-        // 64 34 25 12 22
-        //
-        // Output:
-        // 12 22 25 34 64
-        // Swaps: 7
+        int swaps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swaps++;
+                }
+            }
+        }
 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (i > 0) sb.append(" ");
+            sb.append(arr[i]);
+        }
+        System.out.println(sb);
+        System.out.println("Swaps: " + swaps);
     }
 }
